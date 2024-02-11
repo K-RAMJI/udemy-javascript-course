2fe280f535a183cfeca96b40d88bcef97313b5ab
K-RAMJI <k.ramjics@gmail.com>
2024-02-11 23:58:26 +0530

    Topics covered in section-2
    Hello world program, introduction to JavaScript, Linking JS file to HTML document, Values and variables are covered.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+/* let js='connection';
+if (js==='connection') alert('js file connected.!!');
+
+console.log("Js file connected."); */
+
+/////////////////////////////////////////////////////////////
+
+/* Values and Variables */
+
+console.log("Ramji");
+console.log(23);
+console.log(20+65+84+65);
+
+// Use camelCase for javascript.
+let firstName="Ramji";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let ramji_kumararajan='RM';
+let $function=27;
+
+let person="ramji";
+let PI=3.1415;
+
+//providing meaningful variable names
+let myFirstJob="Programmer";
+let myCurrentJob="Teacher";
+
+//No proper meaning for variable names.
+let job1="programmer";
+let job2="teacher";
+
+console.log(myFirstJob);
+
+
