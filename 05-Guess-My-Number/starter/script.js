52081217b00dfa11395ed988cb4e32a2fcb8ddea
K-RAMJI <k.ramjics@gmail.com>
2024-03-02 23:59:02 +0530

    Covered a topic in section-7
    Covered topic Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector(`.message`));
 
 document.querySelector(`.message`).textContent=`Correct number !!!`;
@@ -8,4 +9,14 @@ document.querySelector(`.number`).textContent=13;
 document.querySelector(`.score`).textContent=10;
 
 document.querySelector(`.guess`).value=23;
-console.log(document.querySelector(`.guess`).value);
+console.log(document.querySelector(`.guess`).value);*/
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+    // console.log(document.querySelector(`.guess`).value);
+    const guess = Number(document.querySelector(`.guess`).value);
+    console.log(guess, typeof (guess));
+
+    if (!guess) {
+        document.querySelector(`.message`).textContent = `NO NUMBER !!!`;
+    }
+});
